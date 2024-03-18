9e09ef0e14e9fc25cf235852a8997332b4c1d7b1
ItsMeAldy <rizaldimustakim10@gmail.com>
2024-03-19 00:42:31 +0800

    belajar view engine pertama

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
--- routes/shop.js
+++ routes/shop.js
@@ -4,10 +4,15 @@ const express = require('express');
 
 const rootDir = require('../util/path');
 
+const adminData = require('./admin');
+
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+  const products = adminData.products;
+  // res.sendFile(path.join(rootDir, 'views', 'shop.ejs'));
+  res.render("shop", {title : 'halo', products : products})
 });
 
 module.exports = router;
+
